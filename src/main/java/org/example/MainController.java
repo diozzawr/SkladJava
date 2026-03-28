7d548877eb38c5048f00e0123153243745d0295e
macbook <bolotbekovamantur3@gmail.com>
2026-03-29 04:26:11 +0600

    product

---
+++ src/main/java/org/example/MainController.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+
+public class MainController {
+    @FXML
+    private TableView<Product> table; // Это связка с fx:id="table" из твоего скрина
+}
